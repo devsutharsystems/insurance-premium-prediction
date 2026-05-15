54c85b97c212b7e5dd42c31e897841ee4070c858
I-am-Dev-Suthar <hello.devsuthar@gmail.com>
2026-05-15 19:02:18 +0530

    refactor: clean insurance project structure

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import streamlit as st
 import requests
 
@@ -42,3 +43,49 @@ if st.button("Predict Premium Category"):
 
     except requests.exceptions.ConnectionError:
         st.error("❌ Could not connect to the FastAPI server. Make sure it's running.")
+=======
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from schema.user_input import UserInput
+from schema.prediction_response import PredictionResponse
+from Model.predict import predict_output, model, MODEL_VERSION
+
+app = FastAPI()
+        
+@app.get("/")
+def home():
+    return {'message': 'Insurance Premium Prediction API'}
+
+@app.get("/health")
+def health_check():
+    return {
+        'status': 'OK',
+        'version': MODEL_VERSION,
+        'model_loaded': model is not None
+    }
+        
+
+@app.post("/predict", response_model=PredictionResponse)
+def predict(data: UserInput):
+
+    user_input = {
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation,
+    }
+
+    try:
+
+        prediction = predict_output(user_input)
+
+        return JSONResponse(status_code=200, content={'response': prediction})
+    
+    except Exception as e:
+        
+        return JSONResponse(status_code=500, content=str(e))
+
+
+>>>>>>> bfacd97 (refactor: clean insurance project structure)
